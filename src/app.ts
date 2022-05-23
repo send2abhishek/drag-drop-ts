f49d90d9fe8509e4d43c58441fa8740f63c0b29a
Abhishek Kumar <send2abhishek@live.com>
2022-05-23 14:53:16 +0530

    feat: Implement es import and export

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,6 @@
-/// <reference path="models/drag-drop.ts" />
-/// <reference path="models/project.ts" />
-/// <reference path="state/project-state.ts" />
-/// <reference path="util/validatable.ts" />
-/// <reference path="decorators/autobind.ts" />
-/// <reference path="components/base-component.ts" />
-/// <reference path="components/project-input.ts" />
-/// <reference path="components/project-item.ts" />
-/// <reference path="components/project-list.ts" />
+import { ProjectInput } from "./components/project-input.js";
+import { ProjectList } from "./components/project-list.js";
 
-namespace App {
-  // Component Base Class
-
-  // ProjectItem Class
-
-  // ProjectInput Class
-
-  new ProjectInput();
-  new ProjectList("active");
-  new ProjectList("finished");
-}
+new ProjectInput();
+new ProjectList("active");
+new ProjectList("finished");
